2c19ef520653db489d7e3a4f60492a7b1ba92981
LilBroCodes <lilbrocodes@gmail.com>
2023-12-11 21:19:35 +0100

    Commit 3 - Changed over to a setup.py based packaging process, added tests/main.py

---
+++ package.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = "DND Controller library using pygame"
+LONG_DESCRIPTION = "Library using pygame that lets you create custom dnd campaigns, with custom Character classes. "
+
+setup(
+    name="dnd_controller",
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    author="LilBroCodes",
+    author_email="lilbrocodes@gmail.com",
+    packages=find_packages(),
+    install_requires=["pygame"],
+    keywords=["DND", "Pygame", "Game controller"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Gaming",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
--- tests/main.py
+++ tests/main.py
@@ -1,1 +1,3 @@
 from src.dnd_controller_LILBROCODES.extras import debug
+
+debug.debug()
